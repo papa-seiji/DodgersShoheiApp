e4c909f6b8459e816839862f0ab9e6f7c878b05d
Seiji Enomoto <iwillbestandbyme@gmail.com>
2024-12-14 13:36:40 +0900

    本番環境デプロイ作業application-test.properties,testdb作成02

--- myProject202411122354/src/test/java/com/example/dodgersshoheiapp/DodgersShoheiAppApplicationTests.java
+++ myProject202411122354/src/test/java/com/example/dodgersshoheiapp/DodgersShoheiAppApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.dodgersshoheiapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class DodgersShoheiAppApplicationTests {
 
 	@Test
 	void contextLoads() {
-		// アプリケーションコンテキストが正しくロードされることを確認
+		// コンテキストのロードが成功することを確認
 	}
 }
