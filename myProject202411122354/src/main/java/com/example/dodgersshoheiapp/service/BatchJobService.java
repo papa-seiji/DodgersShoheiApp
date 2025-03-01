f10f293509ffc091f2be9e91e6cfa36822a3cb93
Seiji Enomoto <iwillbestandbyme@gmail.com>
2025-03-01 19:26:37 +0900

    ログのアーカイブ機能実装

--- myProject202411122354/src/main/java/com/example/dodgersshoheiapp/service/BatchJobService.java
+++ myProject202411122354/src/main/java/com/example/dodgersshoheiapp/service/BatchJobService.java
@@ -20,8 +20,8 @@ public class BatchJobService {
     @Autowired
     private LoginLogoutRepository loginLogoutRepository;
 
-    // @Scheduled(cron = "0 */2 * * * ?") // 2分ごとに実行（開発用）
-    @Scheduled(cron = "0 0 6,18 * * ?") // 毎日 6:00 & 18:00 実行（本番用）
+    @Scheduled(cron = "0 */2 * * * ?") // 2分ごとに実行（開発用）
+    // @Scheduled(cron = "0 0 6,18 * * ?") // 毎日 6:00 & 18:00 実行（本番用）
     public void processLoginLogoutLogs() {
         logger.debug("★★★ バッチ処理開始: processLoginLogoutLogs() ★★★");
 
