c8614159f8d40ce507285d5f5c97c0cec7b674b2
Seiji Enomoto <iwillbestandbyme@gmail.com>
2026-02-02 23:39:01 +0900

    詳細部分もDBのデータが出るように実装#01

--- myProject202411122354/src/main/java/com/example/dodgersshoheiapp/repository/OhtaniGameRepository.java
+++ myProject202411122354/src/main/java/com/example/dodgersshoheiapp/repository/OhtaniGameRepository.java
@@ -5,8 +5,11 @@ import com.example.dodgersshoheiapp.model.OhtaniGameDetail;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
@@ -42,7 +45,8 @@ public class OhtaniGameRepository {
     }
 
     /**
-     * 試合詳細（打席）取得
+     * 試合詳細（打席内容）取得
+     * ★ 新設計：1試合 = 1レコード（pa1〜pa6）
      */
     public List<OhtaniGameDetail> findDetailsByGameId(int gameId) {
 
@@ -50,19 +54,89 @@ public class OhtaniGameRepository {
                     SELECT
                         id,
                         game_id,
-                        batting_order,
-                        pitcher,
-                        pitcher_hand,
-                        result,
-                        created_at
+                        created_at,
+
+                        pa1_pitcher,
+                        pa1_pitcher_hand,
+                        pa1_result,
+                        pa1_description,
+
+                        pa2_pitcher,
+                        pa2_pitcher_hand,
+                        pa2_result,
+                        pa2_description,
+
+                        pa3_pitcher,
+                        pa3_pitcher_hand,
+                        pa3_result,
+                        pa3_description,
+
+                        pa4_pitcher,
+                        pa4_pitcher_hand,
+                        pa4_result,
+                        pa4_description,
+
+                        pa5_pitcher,
+                        pa5_pitcher_hand,
+                        pa5_result,
+                        pa5_description,
+
+                        pa6_pitcher,
+                        pa6_pitcher_hand,
+                        pa6_result,
+                        pa6_description
+
                     FROM ohtani_game_details
                     WHERE game_id = ?
-                    ORDER BY batting_order
                 """;
 
-        return jdbcTemplate.query(
-                sql,
-                new BeanPropertyRowMapper<>(OhtaniGameDetail.class),
-                gameId);
+        return jdbcTemplate.query(sql, ohtaniGameDetailRowMapper, gameId);
     }
+
+    /**
+     * ★ 新規追加：横持ち(pa1〜pa6)専用 RowMapper
+     */
+    private final RowMapper<OhtaniGameDetail> ohtaniGameDetailRowMapper = new RowMapper<>() {
+        @Override
+        public OhtaniGameDetail mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+            OhtaniGameDetail d = new OhtaniGameDetail();
+
+            d.setId(rs.getLong("id"));
+            d.setGameId(rs.getLong("game_id"));
+            d.setCreatedAt(rs.getTimestamp("created_at").toLocalDateTime());
+
+            d.setPa1Pitcher(rs.getString("pa1_pitcher"));
+            d.setPa1PitcherHand(rs.getString("pa1_pitcher_hand"));
+            d.setPa1Result(rs.getString("pa1_result"));
+            d.setPa1Description(rs.getString("pa1_description"));
+
+            d.setPa2Pitcher(rs.getString("pa2_pitcher"));
+            d.setPa2PitcherHand(rs.getString("pa2_pitcher_hand"));
+            d.setPa2Result(rs.getString("pa2_result"));
+            d.setPa2Description(rs.getString("pa2_description"));
+
+            d.setPa3Pitcher(rs.getString("pa3_pitcher"));
+            d.setPa3PitcherHand(rs.getString("pa3_pitcher_hand"));
+            d.setPa3Result(rs.getString("pa3_result"));
+            d.setPa3Description(rs.getString("pa3_description"));
+
+            d.setPa4Pitcher(rs.getString("pa4_pitcher"));
+            d.setPa4PitcherHand(rs.getString("pa4_pitcher_hand"));
+            d.setPa4Result(rs.getString("pa4_result"));
+            d.setPa4Description(rs.getString("pa4_description"));
+
+            d.setPa5Pitcher(rs.getString("pa5_pitcher"));
+            d.setPa5PitcherHand(rs.getString("pa5_pitcher_hand"));
+            d.setPa5Result(rs.getString("pa5_result"));
+            d.setPa5Description(rs.getString("pa5_description"));
+
+            d.setPa6Pitcher(rs.getString("pa6_pitcher"));
+            d.setPa6PitcherHand(rs.getString("pa6_pitcher_hand"));
+            d.setPa6Result(rs.getString("pa6_result"));
+            d.setPa6Description(rs.getString("pa6_description"));
+
+            return d;
+        }
+    };
 }
